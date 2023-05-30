81fc6c1528fa6b488ea0ffddf1fe8f64ec66d1b7
0Ryab <ryansantosinf@gmail.com>
2023-05-30 18:36:59 -0300

    Completo

---
+++ TrabCilene/Exercicio 6 - Lista 2.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+Ryan dos Santos Cirilo
+01.03.2023
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int cont, quantNegativo = 0;
+    float vetor[10],somaPositivo = 0;
+
+        printf("Digite 10 numeros Reais : \n");
+
+            for(cont = 0; cont < 10;  cont++){
+                scanf("%f", &vetor[cont]);
+
+                if(vetor[cont] < 0 ){
+                    quantNegativo++;
+                }
+
+                if(vetor[cont] > 0 ){
+                    somaPositivo += vetor[cont];
+                }
+
+
+            }
+
+            printf("Quantidade negativos: %d\nSoma dos positvos: %.2f",quantNegativo,somaPositivo);
+
+    return 0;
+}
